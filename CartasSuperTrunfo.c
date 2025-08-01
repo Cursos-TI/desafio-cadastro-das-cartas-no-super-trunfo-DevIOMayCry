7675ca1118034937f0695a17ce0810f955c3b041
DevIOMayCry <derikmatheuspastro@gmail.com>
2025-07-31 22:46:22 -0300

    Adicionei as alterações solicitadas para melhorar o código - Estado, números decimais e permitir espaços nos nomes das cidades.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,7 +11,8 @@ int main() {
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     // Nome das variáveis: cod1, cod2, nome1, nome2, pop1, pop2, area1, area2, pib1, pib2, npt1, npt2
 
-    char cod1[4], cod2[4], nome1[50], nome2[50];
+    // Acrescentei o char estado1 e estado2.
+    char cod1[4], cod2[4], nome1[50], nome2[50], estado1[3], estado2[3];
     // char %s
     float area1, area2, pib1, pib2;
     // float %f
@@ -28,49 +29,56 @@ int main() {
 
 
     // A seguir virão todos os inputs da Carta 1;
+    // Estado constando OK - para as 2 cartas
+    // nome da cidade permitindo espaços
+        printf("Carta - Cidade 1 \n");
 
-    printf("Carta - Cidade 1 \n");
+        printf("Crie um código para a Cidade 1: (ex: A01)\n");
+        scanf("%s", cod1);
 
-    printf("Crie um código para a Cidade 1: (ex: A01)\n");
-    scanf("%s", cod1);
+        printf("Digite o nome dessa Cidade: (sem acentos, ex São = Sao)\n");
+        scanf(" %49[^\n]", nome1);
 
-    printf("Digite o nome dessa Cidade: (sem espaços, ex SantaBarbaraDoOeste)\n");
-    scanf("%s", nome1);
+        printf("Digite a sigla do Estado de %s: \n", nome1);
+        scanf("%s", estado1);
     
-    printf("Digite a população de %s:\n", nome1);
-    scanf("%d", &pop1);
+        printf("Digite a população de %s - %s:\n", nome1, estado1);
+        scanf("%d", &pop1);
 
-    printf("Digite a área de %s: (em km², formato americano)\n", nome1);
-    scanf("%f", &area1);
+        printf("Digite a área de %s - %s: (em km², formato americano)\n", nome1, estado1);
+        scanf("%f", &area1);
 
-    printf("Digite o PIB de %s: (em milhões de R$, formato americano)\n", nome1);
-    scanf("%f", &pib1);    
+        printf("Digite o PIB de %s - %s: (em milhões de R$, formato americano)\n", nome1, estado1);
+        scanf("%f", &pib1);    
 
-    printf("Digite o número de Pontos Turísticos de %s:\n", nome1);
-    scanf("%d", &npt1); 
+        printf("Digite o número de Pontos Turísticos de %s - %s:\n", nome1, estado1);
+        scanf("%d", &npt1); 
 
 
     // A seguir virão todos os inputs da Carta 2;
-    printf("__________________________________________ \n");
-    printf("Carta - Cidade 2 \n");
+        printf("__________________________________________ \n");
+        printf("Carta - Cidade 2 \n");
 
-    printf("Crie um código para a Cidade 2: (ex: A01)\n");
-    scanf("%s", cod2);
+        printf("Crie um código para a Cidade 2: (ex: A01)\n"); 
+        scanf("%s", cod2);
 
-    printf("Digite o nome dessa Cidade: (sem espaços, ex SantaBarbaraDoOeste)\n");
-    scanf("%s", nome2);
+        printf("Digite o nome dessa Cidade: (sem acentos, ex São = Sao)\n");
+        scanf(" %49[^\n]", nome2);
     
-    printf("Digite a população de %s:\n", nome2);
-    scanf("%d", &pop2);
+        printf("Digite a sigla do Estado de %s: \n", nome2);
+        scanf("%s", estado2);
 
-    printf("Digite a área de %s: (em km², formato americano)\n", nome2);
-    scanf("%f", &area1);
+        printf("Digite a população de %s - %s:\n", nome2, estado2);
+        scanf("%d", &pop2);
 
-    printf("Digite o PIB de %s: (em milhões de R$, formato americano)\n", nome2);
-    scanf("%f", &pib2);    
+        printf("Digite a área de %s - %s: (em km², formato americano)\n", nome2, estado2);
+        scanf("%f", &area2);
 
-    printf("Digite o número de Pontos Turísticos de %s:\n", nome2);
-    scanf("%d", &npt2); 
+        printf("Digite o PIB de %s - %s: (em milhões de R$, formato americano)\n", nome2, estado2);
+        scanf("%f", &pib2);    
+
+        printf("Digite o número de Pontos Turísticos de %s - %s:\n", nome2, estado2);
+        scanf("%d", &npt2); 
 
 
     // Exibição dos Dados das Cartas:
@@ -81,21 +89,22 @@ int main() {
     // Nome das variáveis: cod1, cod2, nome1, nome2, pop1, pop2, area1, area2, pib1, pib2, npt1, npt2
 
     // Exibir tudo
-    printf("__________________________________________ \n");
-    printf("\nObrigado pelas informações!\n");
-    printf("__________________________________________ \n");
-    printf("CARTA 1 - %s - %s \n", cod1, nome1);
-    printf("População: %d \n", pop1);
-    printf("Área em km²: %f \n", area1);
-    printf("PIB: %f \n", pib1);
-    printf("Número de pontos turísticos: %d \n", npt1);
+    // %d ajustado com .2 para diminuir a quantidade de casas decimais
+            printf("__________________________________________ \n");
+            printf("\nObrigado pelas informações!\n");
+            printf("__________________________________________ \n");
+            printf("CARTA 1 - %s - %s - %s \n", cod1, nome1, estado1);
+            printf("População: %d \n", pop1);
+            printf("Área em km²: %.2f \n", area1);
+            printf("PIB: %.2f \n", pib1);
+            printf("Número de pontos turísticos: %d \n", npt1);
 
-    printf("__________________________________________ \n");
-    printf("CARTA 2 - %s - %s \n", cod2, nome2);
-    printf("População: %d \n", pop2);
-    printf("Área em km²: %f \n", area2);
-    printf("PIB: %f \n", pib2);
-    printf("Número de pontos turísticos: %d \n", npt2);
+            printf("__________________________________________ \n");
+            printf("CARTA 2 - %s - %s - %s \n", cod2, nome2, estado2);
+            printf("População: %d \n", pop2);
+            printf("Área em km²: %.2f \n", area2);
+            printf("PIB: %.2f \n", pib2);
+            printf("Número de pontos turísticos: %d \n", npt2);
 
     return 0;
 }
